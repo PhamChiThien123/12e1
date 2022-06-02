5a541bb4cbe6af98d7b4ab1902f16fd54ba224aa
PhamChiThien123 <100931934+PhamChiThien123@users.noreply.github.com>
2022-06-03 00:01:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "<3",
-    introTitle: 'Do Tuan Dat',
+    introTitle: 'Đỗ Tuấn Đạt',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
